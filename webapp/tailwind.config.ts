7e0e0565b181ea1e87fc9b0ab6b58bf49ade67b8
gaac510 <74474003+gaac510@users.noreply.github.com>
2023-09-18 01:16:20 +1000

    Remove starter template style

--- webapp/tailwind.config.ts
+++ webapp/tailwind.config.ts
@@ -9,15 +9,13 @@ const config: Config = {
     "app/**/*.{js,ts,jsx,tsx,mdx}",
   ].map((path) => srcRoot + "/" + path),
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
+  corePlugins: {
+    /** @todo Enable perflight by removing this. */
+    preflight: false,
+  },
 };
 
 export default config;
